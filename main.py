07a134cf0c98aee44d4ebae376cf27227727f119
Ficus1 <74860665+Ficus1@users.noreply.github.com>
2021-04-07 18:40:46 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 
+import requests
 from flask import Flask, request
 import logging
 import json
@@ -176,6 +177,23 @@ def get_suggests(user_id):
     ]
 
     return suggests
+
+
+def get_country(city_name):
+    try:
+        url = "https://geocode-maps.yandex.ru/1.x/"
+        params = {
+            "apikey": "40d1649f-0493-4b70-98ba-98533de7710b",
+            'geocode': city_name,
+            'format': 'json'
+        }
+        data = requests.get(url, params).json()
+        # все отличие тут, мы получаем имя страны
+        return data['response']['GeoObjectCollection'][
+            'featureMember'][0]['GeoObject']['metaDataProperty'][
+            'GeocoderMetaData']['AddressDetails']['Country']['CountryName']
+    except Exception as e:
+        return e
 
 
 if __name__ == '__main__':
